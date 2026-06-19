96214592b3026c09d259870eb565bc029b1982e2
tappunk <me@tappunk.com>
2026-06-19 21:44:36 +0900

    chore(deps): bump llama-cpp from b9722 to b9724
    Update llama-cpp derivation in nix/common.nix to use upstream
    tag b9724 with corresponding fetchurl hash. Also update flake.lock
    to reflect dependency changes.

--- nix/common.nix
+++ nix/common.nix
@@ -164,11 +164,11 @@ cache=~/.local/npm/cache' > "$userHome/.npmrc"
 
     llama-cpp = pkgs.stdenv.mkDerivation {
       pname = "llama-cpp";
-      version = "b9722";
+      version = "b9724";
 
       src = pkgs.fetchurl {
-        url = "https://github.com/ggml-org/llama.cpp/archive/refs/tags/b9722.tar.gz";
-        hash = "sha256-XZMYdJMyNTQL/TL6Ci/ZU194z7u5momuN1qVGnL1A8g=";
+        url = "https://github.com/ggml-org/llama.cpp/archive/refs/tags/b9724.tar.gz";
+        hash = "sha256-YcMlH+iEUZPMVx6thRvmIvsfMtOFMRPOA5fKrPO9jqE=";
       };
 
       nativeBuildInputs = with pkgs; [ pkg-config cmake ];
